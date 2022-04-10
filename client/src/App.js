a519021fd4ca44261de78040ebe8a307662c7c09
Stephen Ngwu <ngwustephen99@gmail.com>
2022-04-10 13:43:45 +0100

    final deploy

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,19 @@ import { Routes, Route, useNavigate } from "react-router-dom"
 
 import Login from './components/Login';
 import Home from './container/Home';
+import { useEffect } from 'react';
+import { fetchUser } from './utils/fetchUser';
 
 function App() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const user = fetchUser()
+    console.log(user)
+
+    if (!user) navigate("/login")
+  }, [])
+
   return (
     <Routes>
       <Route path="login" element={<Login />} />
